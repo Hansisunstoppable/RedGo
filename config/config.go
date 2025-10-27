fbce80c0d87d71f7545d1415351bddeba13a6477
fanghang <qcxxfh@126.com>
2025-10-27 16:24:39 +0800

    Initial commit

---
+++ config/config.go
@@ -0,0 +1,111 @@
+package config
+
+import (
+	"Godis/lib/logger"
+	"bufio"
+	"io"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+// ServerProperties provides the server configuration
+type ServerProperties struct {
+	Bind           string   `cfg:"bind"`
+	Port           int      `cfg:"port"`
+	AppendOnly     bool     `cfg:"appendOnly"`
+	AppendFilename string   `cfg:"appendFilename"`
+	MaxClients     int      `cfg:"maxClients"`
+	RequirePass    string   `cfg:"requirePass"`
+	Databases      int      `cfg:"databases"`
+	Peers          []string `cfg:"peers"`
+	Self           string   `cfg:"self"`
+}
+
+// Properties 存储全局配置
+var Properties *ServerProperties
+
+// Go 的 init 生命周期函数，会在 main 函数之前自动执行
+func init() {
+	// default config
+	Properties = &ServerProperties{
+		Bind:       "127.0.0.1",
+		Port:       6379,
+		AppendOnly: false,
+	}
+}
+
+// parse parses the configuration file and returns a ServerProperties instance
+func parse(src io.Reader) *ServerProperties {
+	config := &ServerProperties{}
+
+	// read config file
+	rawMap := make(map[string]string)
+	scanner := bufio.NewScanner(src)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) > 0 && line[0] == '#' {
+			continue
+		}
+		pivot := strings.IndexAny(line, " ")
+		if pivot > 0 && pivot < len(line)-1 { // separator found
+			key := line[0:pivot]
+			value := strings.Trim(line[pivot+1:], " ")
+			rawMap[strings.ToLower(key)] = value
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		logger.Fatal(err)
+	}
+
+	// parse format
+	t := reflect.TypeOf(config)
+	v := reflect.ValueOf(config)
+	n := t.Elem().NumField()
+	for i := 0; i < n; i++ {
+		field := t.Elem().Field(i)
+		fieldVal := v.Elem().Field(i)
+		key, ok := field.Tag.Lookup("cfg")
+		if !ok {
+			key = field.Name
+		}
+		value, ok := rawMap[strings.ToLower(key)]
+		if ok {
+			// fill config
+			switch field.Type.Kind() {
+			case reflect.String:
+				fieldVal.SetString(value)
+			case reflect.Int:
+				intValue, err := strconv.ParseInt(value, 10, 64)
+				if err == nil {
+					fieldVal.SetInt(intValue)
+				}
+			case reflect.Bool:
+				boolValue := "yes" == value
+				fieldVal.SetBool(boolValue)
+			case reflect.Slice:
+				if field.Type.Elem().Kind() == reflect.String {
+					slice := strings.Split(value, ",")
+					fieldVal.Set(reflect.ValueOf(slice))
+				}
+			}
+		}
+	}
+	return config
+}
+
+// SetupConfig 从文件名读取 configuration
+func SetupConfig(configFilename string) {
+	file, err := os.Open(configFilename)
+	if err != nil {
+		panic(err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+
+		}
+	}(file)
+	Properties = parse(file)
+}
