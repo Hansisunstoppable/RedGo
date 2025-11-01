a44acb9b1f4c13dffbf6b4270c454edb15303f09
fanghang <qcxxfh@126.com>
2025-11-01 22:14:53 +0800

    fix: Ensure DB Index is reset to 0 after AOF loading
    AOF loading now guarantees that the final selected database index is 0.

--- aof/aof.go
+++ aof/aof.go
@@ -11,11 +11,13 @@ import (
 	"io"
 	"os"
 	"strconv"
+	"strings"
 )
 
 type CmdLine [][]byte
 
 const aofBufferSize = 1 << 16 //  AOF 通道的缓冲区大小
+const initDBIndex = 0
 
 // payload 待写入的 aof 命令 与 对应的数据库编号
 type payload struct {
@@ -36,14 +38,13 @@ func NewAofHandler(db database.Database) (*AofHandler, error) {
 	handler := &AofHandler{}
 	handler.aofFileName = config.Properties.AppendFilename
 	handler.db = db
-	handler.LoadAof() // 将之前的 AOF 文件加载到数据库中（执行所有 AOF 命令来恢复）
 	aofFile, err := os.OpenFile(handler.aofFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0600)
 	if err != nil {
 		return nil, err
 	}
 	handler.aofFile = aofFile
 	handler.aofChan = make(chan *payload, aofBufferSize)
-
+	handler.LoadAof() // 将之前的 AOF 文件加载到数据库中（执行所有 AOF 命令来恢复）
 	// 启动一个协程，读取管道内容，写入 AOF 文件（异步写入）
 	go func() {
 		handler.handleAof()
@@ -70,7 +71,9 @@ func (h *AofHandler) LoadAof() error {
 		logger.Error("AOF file open error: " + err.Error())
 		return err
 	}
-	defer aofFile.Close()
+	defer func() {
+		aofFile.Close()
+	}()
 
 	ch := parser.ParseStream(aofFile)    // 解析 AOF 文件得到 payload， 送入 ch
 	fakeConn := &connection.Connection{} // 构造一个空 client ，后续若执行到 SELECT 命令，通过 execSelect 传入 selectedDB 字段
@@ -94,11 +97,33 @@ func (h *AofHandler) LoadAof() error {
 			logger.Error("AOF file require multi bulk reply")
 			continue
 		}
+
+		// 若遇到了 select 命令，则要更新 h.currentDB
+		cmdName := strings.ToLower(string(r.Args[0]))
+		if cmdName == "select" {
+			if len(r.Args) != 2 {
+				logger.Error("Invalid DB index in SELECT command: " + err.Error())
+			}
+			dbIndex, err := strconv.Atoi(string(r.Args[1]))
+			if err != nil {
+				logger.Error("Invalid DB index in SELECT command: " + err.Error())
+				continue
+			}
+			h.currentDB = dbIndex
+		}
+
 		// exec 方法需要传入一个 client，用于传递执行命令后的响应。但这里无需响应，只需保存 selectedDB（当前选择的数据库编号）
-		logger.Debug("AOF execute command: %s", r.Args)
 		rep := h.db.Exec(fakeConn, r.Args)
 		if reply.IsError(rep) {
 			logger.Error("Execute AOF command error")
+		}
+	}
+	// 初始默认连接 0 号数据库; 若执行完 aof 之后不在 0 号数据库，则需要向 aof 写入 select 0
+	if h.currentDB != 0 {
+		data := reply.MakeMultiBulkReply(util.ToCmdLime("SELECT", strconv.Itoa(initDBIndex))).ToBytes()
+		_, err := h.aofFile.Write(data)
+		if err != nil {
+			logger.Error("write aof file error: " + err.Error())
 		}
 	}
 
@@ -109,7 +134,7 @@ func (h *AofHandler) LoadAof() error {
 func (h *AofHandler) handleAof() {
 	// 先判断是否产生数据库切换
 	// 若需要切换，要将额外的 Select 命令写入到 aof 文件，以供未来恢复的时候执行
-	h.currentDB = 0
+	// h.currentDB = 0 无需初始化为 0，因为一开始有可能不是 0
 	for p := range h.aofChan {
 		if p.dbIndex != h.currentDB { // 数据库产生切换，要向 aof 文件写入 SELECT dbIndex 命令
 			h.currentDB = p.dbIndex                                                                       // 更新当前数据库序号
@@ -123,7 +148,6 @@ func (h *AofHandler) handleAof() {
 
 		// 将命令本身 写入 aof 文件
 		data := reply.MakeMultiBulkReply(p.cmd).ToBytes()
-		// logger.Info("write aof: " + string(data))
 		_, err := h.aofFile.Write(data)
 		if err != nil {
 			logger.Error("write aof file error: " + err.Error())
